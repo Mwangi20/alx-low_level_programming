4da3f3523e16e4c12bf56cf09345e698cdd53833
John Mwangi <johnmkamau25@gmail.com>
2023-02-26 23:06:27 -0800

    1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps the values of two integers
+ * @a: first value to swap
+ * @b: second value to swap
+ * Return: nothing
+ */
+
+void swap_int(int *a, int *b)
+{
+	int value = *a;
+	*a = *b;
+	*b = value;
+}
